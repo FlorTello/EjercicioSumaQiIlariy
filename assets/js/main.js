f2f8517d6fa37381a1f0092cf60ca57c9462f684
Naomi Villanueva <naovillaj@gmail.com>
2017-03-01 13:10:51 -0500

    Funcion Operacion 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
-alert();
+function operacion(){
+
+	var resultado = document.getElementById("resultado");
+	var operaciones = document.getElementById("operacion").value;
+
+	switch (operaciones) {
+
+	case "suma":
+		resultado = document.getElementById("num1").value + document.getElementById("num2").value;
+		break;
+
+	case "resta":
+		resultado = document.getElementById("num1").value - document.getElementById("num2").value;
+		break;	
+
+	case "multiplicacion":
+		resultado = document.getElementById("num1").value * document.getElementById("num2").value;
+		break;
+
+	case "division":
+		resultado = document.getElementById("num1").value / document.getElementById("num2").value;
+		break;
+
+	case "potencia":
+		resultado = Math.pow(document.getElementById("num1").value , document.getElementById("num2").value);
+		break;
+
+	case "raiz":
+		resultado = Math.pow(document.getElementById("num1").value , (1/document.getElementById("num2").value));	
+		break;
+
+	default:
+		resultado = "Operación no registrada";
+
+	}
+
+}
