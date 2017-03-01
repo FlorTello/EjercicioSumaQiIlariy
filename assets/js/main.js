c30df8a208e34ed1a85cbf972a0c0226cd4e7ce8
Mary <mary20bw@gmail.com>
2017-03-01 13:22:06 -0500

    boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-alert();
+         functionMostrarResultado(){
+           var n1=document.getElementById("num1").value;
+          var nombre= document.getElementById();
+          var r=document.getElementById("resultado");
+
+          var respuesta= nombre + "el resultado de la operacion es" + r;
+          r.innerHTML=respuesta;
+
+
+
+
+       }
