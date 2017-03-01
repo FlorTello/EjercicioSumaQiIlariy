5d707277f8f80a6105a0c2a2e98ba2062f391863
Natha <natha.astolar@gmail.com>
2017-03-01 12:26:46 -0500

    Primer Commit de natha

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-alert();
+function continuar(operar){
+	//if(operar == true){
+	//	return nuevonumero 
+	//}
+		
+		if(confirm("Si")){
+			return document.getElementById('number1').value;
+		}
+		else{
+			return document.getElementById('limpiar')
+		}
+	
+}
+
