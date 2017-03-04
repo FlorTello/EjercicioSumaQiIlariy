b653fd45a074321cc699181176cab1f487586f2d
Emma Tapia <clioemma_28@hotmail.com>
2017-03-03 20:45:39 -0500

    Tercer commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,39 @@
-function
+function operar(num1,num2,operacion){
+
+	var resultado;
+
+	//Realizar operacion
+	switch(operacion){
+		case("1"): resultado = parseInt(num1) + parseInt(num2); break;
+		case("2"): resultado = num1 - num2; break;
+		case("3"): resultado = num1 * num2; break;
+		case("4"): resultado = (num1 / num2).toFixed(2); break;
+		case("5"): resultado = Math.pow(num1,num2); break;
+		case("6"): resultado = (Math.pow(num1,(1/num2))).toFixed(2);break;
+		default: resultado = "Operación no registrada";
+	}
+	document.getElementById("num1").value = resultado;
+	return resultado;
+}
+
+
+var result;
+function calcular(){
+	var number1 = document.getElementById("num1").value;
+	var number2 = document.getElementById("num2").value;
+	var opcion = document.getElementById("operacion").value;
+	result = operar(number1,number2,opcion);
+	var continuar = prompt("desea continuar \n 0 = No  \ 1 = Si");	
+	if(continuar == "1"){
+		 	
+		 document.getElementById("num2").value ="";
+		 document.getElementById("operacion").value ="";
+	}
+	else{
+		alert("El resultado total es : " + result);
+		var limpiar = document.getElementById("limpiar").value;
+		limpiar();
+
+	}
+
+}
